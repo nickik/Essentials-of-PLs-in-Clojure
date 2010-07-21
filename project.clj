ea8ef6a7df0b28c2da82eb13f6cda0a30a9af6ee
nickik <nickik@gmx.ch>
2010-07-22 01:22:17 +0200

    Basic stuff0

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject EssentialsOfProgrammingLanguages "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [trammel "0.1.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-difftest "1.2.2"]])
+
