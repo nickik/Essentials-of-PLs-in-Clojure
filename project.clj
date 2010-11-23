20878fb09e6825b69844fea9fa2722c957d54462
nickik <nickik@gmx.ch>
2010-11-23 21:41:32 +0100

    start again

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject EssentialsOfProgrammingLanguages "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [trammel "0.1.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-difftest "1.2.2"]
                      [clj-stacktrace "0.2.0"]])
